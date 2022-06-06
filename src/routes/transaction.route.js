4d034feb91ce14077d3ea1ff49babe2e71e77d39
Mathias Angule <angulemathias3@gmail.com>
2022-06-06 14:39:15 +0300

    create new transaction

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -6,4 +6,7 @@ const transactionController = require("../controllers/transaction.controller");
 // get all transactions
 router.get("/", transactionController.getTransactions);
 
+// create new transaction
+router.post("/", transactionController.createTransaction);
+
 module.exports = router;
